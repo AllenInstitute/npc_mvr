b29691f0d9e2d3d1caf659a6c047a1b215f74168
bjhardcastle <bjhardcastle@users.noreply.github.com>
2024-04-30 01:15:18 +0000

    Bump

--- src/npc_mvr/mvr.py
+++ src/npc_mvr/mvr.py
@@ -513,13 +513,17 @@ def get_video_frame_times(
     correct_sync_line_name_mapping = get_camera_sync_line_name_mapping(
         sync_path_or_dataset, *videos
     )
-    if tuple(get_camera_name_on_sync(c) for c in correct_sync_line_name_mapping) != tuple(correct_sync_line_name_mapping.values()):
+    if tuple(
+        get_camera_name_on_sync(c) for c in correct_sync_line_name_mapping
+    ) != tuple(correct_sync_line_name_mapping.values()):
         logger.warning(
             f"Camera lines are plugged into sync incorrectly - we'll accommodate for this, but if this is a recent session check the rig: {correct_sync_line_name_mapping}"
         )
     camera_exposing_times = get_cam_exposing_times_on_sync(sync_path_or_dataset)
     camera_exposing_times = {
-        camera: camera_exposing_times[get_camera_name(correct_sync_line_name_mapping[camera])]
+        camera: camera_exposing_times[
+            get_camera_name(correct_sync_line_name_mapping[camera])
+        ]
         for camera in camera_exposing_times
     }
     frame_times: dict[upath.UPath, npt.NDArray[np.floating]] = {}
